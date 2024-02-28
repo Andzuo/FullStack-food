02142addfddf747caa80b646af4ddeb08015f297
Andzuo <alucasm02@gmail.com>
2024-02-27 22:17:00 -0300

    Fix: resolvido book com o mutateQuery

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         )}
       </div>
         {isModalOpen && <CreateModal/>}
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOpenModal}>Adicionar</button>
+        <button className="mt-10 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleOpenModal}>Adicionar</button>
     </div>
   )
 }
